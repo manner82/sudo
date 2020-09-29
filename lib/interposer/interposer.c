328130d520d41b18a946fc474ed1ad8198333253
Laszlo Orban <laszlo.orban@quest.com>
2020-09-29 11:30:52 +0200

    add interposer module to intercept execve calls

---
+++ lib/interposer/interposer.c
@@ -0,0 +1,22 @@
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <dlfcn.h>
+#include <stdio.h>
+
+__attribute__((__visibility__("default"))) int
+execve(const char *command, char * const argv[], char * const envp[])
+{
+    int (*execve_fn)(const char *, char *const *, char *const *);
+
+    /* Find the "next" execve function in the dynamic chain. */
+    execve_fn = dlsym(RTLD_NEXT, "execve");
+    if (execve_fn == NULL) {
+        return -1;              /* should not happen */
+    }
+
+    printf("[INTERPOSER] -> running command: %s\n", command);
+
+    /* Execute the command using the "real" execve function. */
+    return (*execve_fn)(command, argv, envp);
+}
