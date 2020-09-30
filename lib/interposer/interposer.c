8f2a1722f46d6fdbbb8f6dba44959a6af996e45b
Robert Manner <robert.manner@balabit.com>
2020-09-30 09:48:36 +0200

    fixup! add interposer module to intercept execve calls

--- lib/interposer/interposer.c
+++ lib/interposer/interposer.c
@@ -7,10 +7,42 @@
 #include <unistd.h>
 #include <dlfcn.h>
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+
+
+static int ipc_fd = -1;
+
+
+__attribute__((constructor)) void
+interposer_init(void)
+{
+    const char *ipc_rc_str = getenv("SUDO_IPC_FD");
+    if (ipc_rc_str == NULL) {
+        fprintf(stderr, "Failed to get SUDO_IPC_FD\n");
+        exit(129); // TODO
+    }
+
+    char *end = NULL;
+    ipc_fd = strtol(ipc_rc_str, &end, 10);
+    if (end == NULL || *end != '\0') {
+        fprintf(stderr, "Failed to init: SUDO_IPC_FD contained invalid value '%s'\n", ipc_rc_str);
+        exit(129); // TODO
+    }
+
+    fprintf(stderr, "[INTERPOSER] -> will communicate on descriptor %d\n", ipc_fd);
+}
+
 
 sudo_dso_public int
 execve(const char *command, char * const argv[], char * const envp[])
 {
+    if (ipc_fd < 0) {
+        fprintf(stderr, "Failed to initialize sudo interposer\n"); //
+        errno = EPERM;
+        return -1;
+    }
+
     int (*execve_fn)(const char *, char *const *, char *const *);
 
     /* Find the "next" execve function in the dynamic chain. */
